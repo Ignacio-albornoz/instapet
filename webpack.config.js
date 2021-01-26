9667191ee541206feb88db943026c406650c0225
NachoEscritorio <ignaciog.albornoz@gmail.com>
2021-01-25 23:17:53 -0300

    Pre Deploy Vercel

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
